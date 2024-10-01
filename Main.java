fe08e0c7a380e4414d5102d03a80fd0ed158f583
Enzo Xavier <e.pecantet@edu.pucrs.br>
2024-10-01 19:04:24 -0300

    teste 2 para o grupo

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Testando git para o grupo");
+        System.out.println("Testando git para o grupo 2");
     }
 }
